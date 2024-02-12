f77aa9462e76019c1e9a07143b4f54776eac4def
Fanasina95 <159781294+Fanasina95@users.noreply.github.com>
2024-02-12 16:20:04 +0300

    function

--- inc/Function.php
+++ inc/Function.php
@@ -3,13 +3,13 @@
     
     function checkAdmine($pseudo,$mdp)
     {
-        $sql="SELECT * FROM achat ";
+        $sql="SELECT * FROM admine Where %s";
         $sql=sprintf($sql,$pseudo,$mdp);
         $resultat=mysqli_query(dbconnect(),$sql);
         $nombre=mysqli_num_rows($resultat);
         if ($nombre==1) {
             while($donne=mysqli_fetch_assoc($resultat)){
-                $id=$donne['id'];
+                $id=$donne['id_admin'];
             }
             return $id;
         }
