751d7b839b6b9ec0897a63c01986a2078cd4b07c
Fanasina95 <159781294+Fanasina95@users.noreply.github.com>
2024-02-12 21:08:55 +0300

    base

--- inc/Function.php
+++ inc/Function.php
@@ -80,9 +80,57 @@
         $resultat=mysqli_query(dbconnect(),$sql);
     }
 
-    function getPartielle()
+    function setcueilleurs($id,$nom,$genre,$date)
     {
-        $sql="SELECT * FROM partielle";
+        $sql="INSERT INTO cueilleurs VALUES (%d,'%s','%s','%s')";
+        $sql=sprintf($sql,$id,$nom,$genre,$date);
+        
+        $resultat=mysqli_query(dbconnect(),$sql);
+    }
+
+    function setcategorie($id,$nom)
+    {
+        $sql="INSERT INTO categorie VALUES (%d,'%s')";
+        $sql=sprintf($sql,$id,$nom);
+        
+        $resultat=mysqli_query(dbconnect(),$sql);
+    }
+
+    function setdepense($id,$idc,$montant,$date)
+    {
+        $sql="INSERT INTO depense VALUES (%d,%d,'%s','%s')";
+        $sql=sprintf($sql,$id,$idc,$montant,$date);
+        
+        $resultat=mysqli_query(dbconnect(),$sql);
+    }
+
+    function setsalaire($id,$montant)
+    {
+        $sql="INSERT INTO salaire VALUES (%d,'%s')";
+        $sql=sprintf($sql,$id,$montant);
+        
+        $resultat=mysqli_query(dbconnect(),$sql);
+    }
+
+    function setparcelle($num,$surface,$id)
+    {
+        $sql="INSERT INTO parcelle VALUES (%d,%d,%d)";
+        $sql=sprintf($sql,$num,$surface,$id);
+        
+        $resultat=mysqli_query(dbconnect(),$sql);
+    }
+
+    function setresulat($poidt,$poidr,$cout)
+    {
+        $sql="INSERT INTO resulat VALUES (%d,%d,'%s')";
+        $sql=sprintf($sql,$poidt,$poidr,$cout);
+        
+        $resultat=mysqli_query(dbconnect(),$sql);
+    }
+
+    function getresulat()
+    {
+        $sql="SELECT * FROM resulat";
         $resultat=mysqli_query(dbconnect(),$sql);
         return getContent($resultat);
     }
@@ -90,6 +138,41 @@
     function getdu_the()
     {
         $sql="SELECT * FROM du_the";
+        $resultat=mysqli_query(dbconnect(),$sql);
+        return getContent($resultat);
+    }
+
+    function getcueilleurs()
+    {
+        $sql="SELECT * FROM cueilleurs";
+        $resultat=mysqli_query(dbconnect(),$sql);
+        return getContent($resultat);
+    }
+
+    function getcategorie()
+    {
+        $sql="SELECT * FROM categorie";
+        $resultat=mysqli_query(dbconnect(),$sql);
+        return getContent($resultat);
+    }
+
+    function getdepense()
+    {
+        $sql="SELECT * FROM depense";
+        $resultat=mysqli_query(dbconnect(),$sql);
+        return getContent($resultat);
+    }
+
+    function getsalaire()
+    {
+        $sql="SELECT * FROM salaire";
+        $resultat=mysqli_query(dbconnect(),$sql);
+        return getContent($resultat);
+    }
+
+    function getresulat()
+    {
+        $sql="SELECT * FROM resulat";
         $resultat=mysqli_query(dbconnect(),$sql);
         return getContent($resultat);
     }
