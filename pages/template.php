1fdea82d656823668f8d398adf11e14712373401
fenoantra <106917754+fenoantra@users.noreply.github.com>
2024-02-13 09:06:00 +0100

    front-suite

--- pages/template.php
+++ pages/template.php
@@ -7,7 +7,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" type="image/png" href="../Assets/image/logo.png">
+    <link rel="shortcut icon" type="image/png" href="../Assets/image/icon.png">
     <link rel="stylesheet" href="../Assets/style.css">
     <link href="../Assets/css/bootstrap.min.css" rel="stylesheet">
     <script src="../Assets/js/jquery.min.js"></script>
@@ -15,10 +15,17 @@
     <title>Recolte</title>
 
 </head>
-<body>
+<body style="background: url(../Assets/image/hehe.jpg) ; background-repeat : no-repeat; background-size : cover;">
+
 <nav class="navbar navbar-inverse">
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
+      <ul class="nav navbar-nav navbar-left">
+          <div style ="color = black">
+          <li><img src="../Assets/image/icon.png" width=20px height=20px> </li> 
+          <li><p style = "color : white; font-size: 15px; font-family: greating; float right "> Recolte</p></li>
+          </div>
+      </ul>
         <li><a href="template.php?page=variete">Variete</a></li>
         <li><a href="template.php?page=parcelle">Parcelles</a></li>
         <li><a href="template.php?page=cueilleur">Cueilleurs</a></li>
@@ -29,7 +36,10 @@
   </div>
 </nav>
 
-<center> <img src="../Assets/image/logo.png" width=80px height=80px></center>
+<center>
+
+</center>
+
 <?php
   include($page);
 ?>
